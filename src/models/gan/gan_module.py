a6e06053730115980343166ce1b0184b4a72d467
thivux <hanchungluong@gmail.com>
2023-05-30 16:26:33 +0700

    log images to wandb

--- src/models/gan/gan_module.py
+++ src/models/gan/gan_module.py
@@ -1,4 +1,7 @@
-from typing import Any, List
+from torchvision.transforms import ToPILImage
+import torchvision.utils as vutils
+
+from typing import Any
 
 import torch
 from torch.optim import Adam
@@ -27,10 +30,12 @@ class GANLitModule(LightningModule):
 
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.generator(x)
+        return self.generator(x) # [len(x), 784]
 
-    def get_noise(self, z_dim, n_samples):
-        return torch.randn(n_samples, z_dim, device=self.generator.device)
+    def get_noise(self, z_dim, n_samples, fixed_noise=False):
+        if fixed_noise:
+            torch.manual_seed(7749)
+        return torch.randn(n_samples, z_dim, device=self.generator.device) # [n_samples, z_dim]
 
     def get_disc_loss(self, real, fake):
         fake_pred = self.discriminator(fake.detach()) 
@@ -46,6 +51,17 @@ class GANLitModule(LightningModule):
         gen_loss = self.criterion(fake_pred, torch.ones_like(fake_pred))
         return gen_loss
     
+    def on_train_start(self) -> None:
+        # cache fixed noise for visualization
+        self.fixed_noise = self.get_noise(z_dim=self.hparams.z_dim, n_samples=25, fixed_noise=True)
+
+        # log sample real images for visualization
+        train_loader = self.trainer.train_dataloader
+        x, _ = next(iter(train_loader))
+        samples = x[:25]
+        grid = vutils.make_grid(samples, nrow=5, normalize=True)
+        self.logger.log_image(key='real images', images=[ToPILImage()(grid)])
+
     def training_step(self, batch: Any, batch_idx: int, optimizer_idx: int):
         # flatten the input images
         x, y = batch
@@ -62,6 +78,12 @@ class GANLitModule(LightningModule):
             disc_loss = self.get_disc_loss(real, fake)
             self.log('disc_loss', disc_loss, on_step=False, on_epoch=True, prog_bar=True)
             return disc_loss
+
+    def on_train_epoch_end(self, ) -> None:
+        sample_fakes_flatten = self.generator(self.fixed_noise) # [25, 784]
+        sample_fakes = sample_fakes_flatten.view(-1, 1, 28, 28) # [25, 1, 28, 28]
+        grid = vutils.make_grid(sample_fakes, nrow=5, normalize=True) 
+        self.logger.log_image(key='fake images', images=[ToPILImage()(grid)])
 
     def test_step(self, batch: Any, batch_idx: int):
         pass
